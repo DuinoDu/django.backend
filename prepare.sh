ce90e1a1bbcedf693bdac0761bc369907dc6c88e
DuinoDu <472365351duino@gmail.com>
2018-07-08 01:47:46 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,8 @@ pip install -r requirements.txt
 ./manage.py makemigrations simple_app
 ./manage.py migrate
 
+echo ""
 echo "------------"
-echo "./manage.py makesuperuser"
+echo "./manage.py createsuperuser"
 echo "------------"
 echo "./manage.py runserver"
