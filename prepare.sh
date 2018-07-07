e9fd0a68a5d14c461287fb2cef05bb24a403588c
DuinoDu <472365351duino@gmail.com>
2018-07-08 01:41:54 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,7 @@
-# django.backend
-```
-git clone https://github.com/duinodu/django.backend
-cd django.backend
+#!/bin/bash -e
+
 pip install -r requirements.txt
 ./manage.py makesuperuser
 ./manage.py makemigrations simple_app
 ./manage.py migrate
 ./manage.py runserver
-```
-Login to admin and add some data.
